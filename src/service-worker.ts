71b904ddce1de2332a392d7bed4020ee8d6c0cd3
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2021-08-02 03:00:12 +0500

    add three axtra method to service worker

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -95,3 +95,34 @@ self.addEventListener('install', (e) => {
       await cache.addAll(urlToCached);
     })());
   });
+
+  // Cache and return requests
+self.addEventListener('fetch', event => {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          // Cache hit - return response
+          if (response) {
+            return response;
+          }
+          return fetch(event.request);
+        }
+      )
+    );
+  });
+  
+  // Update a service worker
+  self.addEventListener('activate', event => {
+    var cacheWhitelist = ['pwa-task-manager'];
+    event.waitUntil(
+      caches.keys().then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+  });
